34d132abce8803e78cf070bc50fedc52580dc0b3
yashaskor <ya1985.85@gmail.com>
2023-05-01 23:59:43 +0300

    feat: create keyboard layout, add listeners, change languages, save state of language after reload

--- keyboard.js
+++ keyboard.js
@@ -26,4 +26,176 @@ const text = document.createElement('h3');
             
 document.body.appendChild(title);
 document.body.appendChild(output);
+
+   
+const buttonHolder = {
+    en: {},
+    ru: {}
+};
+
+let currentLayout = readLang() || 'en';
+let capsLockEnabled = false;
+
+function createButton(key) {
+    const button = document.createElement('button');
+    button.textContent = key;
+
+    if (key === "Tab") {
+        button.classList.add ('long');
+    }
+    if (key === "Backspace") {
+        button.classList.add ('wide');
+    }
+    if (key === "CapsLock") {
+        button.classList.add ('longer');
+    }
+    if (key === "Enter") {
+        button.classList.add ('wide');
+    }
+    if (key === "Shift") {
+        button.classList.add ('longest');
+    }
+    if (key === "Control") {
+        button.classList.add ('longer');
+    }
+    if (key === "Del") {
+        button.classList.add ('small');
+    }
+    if (key === "Alt") {
+        button.classList.add ('small');
+    }
+    if (key === "Space") {
+        button.classList.add ('extra-wide');
+    }
+    if (key === "RightShift") {
+        button.classList.add ('small');
+        button.textContent = ("Shift");
+    }
+
+    button.addEventListener('click', () => {
+        handleKeyPress(key);
+    });
+    button.addEventListener('mousedown', () => {
+        button.classList.add('active');
+    });
+    button.addEventListener('mouseup', () => {
+        button.classList.remove('active');
+    });
+    buttonHolder[currentLayout][key] = button;
+    return button;
+}
+
+function createRow(keys) {
+    const row = document.createElement('div');
+    row.classList.add('keyboard-row');
+    keys.forEach(key => {
+        const button = createButton(key);
+        row.appendChild(button);
+    });
+    return row;
+}
+
+function createKeyboard(layout) {    
+    const keyboard = document.createElement('div');
+    keyboard.id = ("keyboard");    
+    // keyboard.innerHTML = '';
+    layout.forEach(row => {
+        const keyboardRow = createRow(row);
+        keyboard.appendChild(keyboardRow);
+    });    
+    document.body.appendChild(keyboard);
+}
+
+function handleKeyPress(key) {
+    // debugger
+    switch (key) {
+        case 'Backspace':
+            output.value = output.value.slice(0, -1);
+            break;
+        case 'CapsLock':
+            capsLockEnabled = !capsLockEnabled;
+            keyboard.querySelectorAll('button').forEach(button => {
+                if (button.textContent.length === 1) {
+                    button.textContent = capsLockEnabled ? button.textContent.toUpperCase() : button.textContent.toLowerCase();
+                }
+            });
+            break;
+        case 'Shift':
+            // do nothing, the shift key is only used for changing the keyboard layout
+            // if the shift key is pressed, switch to the other keyboard layout
+            currentLayout = currentLayout === 'en' ? 'ru' : 'en';
+            //add lang to query string
+            setLang(currentLayout);
+            createKeyboard(keyboardLayouts[currentLayout]);
+            break;
+        case 'Control':
+
+            break;
+        case 'Alt':
+
+            break;
+        case 'Enter':
+            output.value += '\n';
+            break;
+        case 'Space':
+            output.value += ' ';
+            break;
+        case 'Tab':
+            output.value += '    ';
+            break;
+        default:
+            output.value += capsLockEnabled ? key.toUpperCase() : key.toLowerCase();
+            break;
+    }
+}
+
+function readLang() {
+    const urlParams = new URLSearchParams(document.location.search);
+    return urlParams.get('lang');
+}
+
+function setLang(lang) {
+    const urlParams = new URLSearchParams(document.location.search);
+    urlParams.set('lang', lang);
+    document.location.search = urlParams.toString();
+}
+
+createKeyboard(keyboardLayouts[currentLayout]);
+
+document.addEventListener('keydown', event => {
+    const key = event.key.toLowerCase();
+
+    if (key === 'shift') {
+        // if the shift key is pressed, switch to the other keyboard layout
+        currentLayout = currentLayout === 'en' ? 'ru' : 'en';
+        // add lang to query string
+        setLang(currentLayout);
+        createKeyboard(keyboardLayouts[currentLayout]);
+    } else {
+        // find the corresponding button and simulate a click event
+        const button = buttonHolder[currentLayout][key]
+        if (button) {
+            button.click();
+        }
+    }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    output.focus();
+});
+
+// add keyboard press listener
+document.addEventListener('keydown', event => {
+    const key = event.key;
+    // add active class to the button
+    const button = buttonHolder[currentLayout][key]
+    if (button) {
+        button.classList.add('active');
+        // timeout to remove the active class
+        setTimeout(() => {
+            button.classList.remove('active');
+        }, 100);
+    }
+});
+
 document.body.appendChild(text);
